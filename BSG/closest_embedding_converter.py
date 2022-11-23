beba566bd839832c4401822a2d745a702745fed8
Adrian Campoy <adriancampoy@Oculus-Pacific-24f61ae5c7df4d6f.localdomain>
2022-11-23 16:31:51 +0100

    Adding distances in the embeddings csv

--- BSG/closest_embedding_converter.py
+++ BSG/closest_embedding_converter.py
@@ -19,17 +19,19 @@ def compute_distance(input_data,
     index.prepare()
 
     product_numbers = embeddings[product_no_column_name].to_list()
-    output_df = pd.DataFrame(columns=['id', 'output'])
+    output_df = pd.DataFrame(columns=['id', 'closest_items', 'distances'])
     for x in tqdm(range(len(embeddings))):
         neighbors, distances = index.query(embeddings.loc[x, embedding_column_name].reshape(1, -1), k=n_outputs)
         neighbors = neighbors[0].tolist()
         neighbors = neighbors[1:]  # We remove the first element since it is the article we are querying itself.
         # Not necessary if the "index" has not been trained with the vector with which we are querying.
+        distances = distances[0].tolist()
+        distances = distances[1:]
         closest_products = [product_numbers[x] for x in
                             neighbors]  # TODO: figure out a way to do this step more efficient
         product = embeddings.loc[x, product_no_column_name]
-        new_output = pd.DataFrame([{'id': product, 'output': closest_products}])
+        new_output = pd.DataFrame([{'id': product, 'closest_items': closest_products, 'distances': distances}])
         output_df = pd.concat([output_df, new_output], ignore_index=True)
 
-    output_df['output'] = output_df['output'].astype(str)
+    output_df['closest_items'] = output_df['closest_items'].astype(str)
     return output_df
